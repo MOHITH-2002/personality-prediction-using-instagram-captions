6b13d0deb169bab3a5eb2e16fa09c3b5b7008d4e
Mohith B A <mohithba9353@gmail.com>
2024-06-28 22:30:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,65 @@ from model import load_model_vectorizer_encoder, GetInstagramProfile, clear_text
 
 app = Flask(__name__)
 
+# # Load model, vectorizer, and encoder
+# model_path = 'linear_svc_model.pkl'
+# vectorizer_path = 'tfidf_vectorizer.pkl'
+# encoder_path = 'label_encoder.pkl'
+# model, vectorizer, encoder = load_model_vectorizer_encoder(model_path, vectorizer_path, encoder_path)
+
+# # Initialize Instagram profile retrieval class
+# cls = GetInstagramProfile()
+
+# # Text cleaning function
+# def clean_user_posts(username):
+#     # Get user's Instagram captions
+#     user_df = cls.get_post_info_csv(username)
+
+#     # Clean the text data
+#     user_posts, _ = clear_text(user_df['posts'])
+
+#     return user_posts
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/', methods=['POST'])
+# def predict():
+#     username = request.form['username']
+
+#     if not username:
+#         return jsonify({'error': 'Username not provided.'}), 400
+
+#     try:
+#         # Clean user posts
+#         user_posts = clean_user_posts(username)
+
+#         # Vectorize user posts
+#         user_post = vectorizer.transform(user_posts).toarray()
+
+#         # Predict user personality
+#         user_pred = model.predict(user_post)
+
+#         # Decode predicted personality
+#         user_personality = encoder.inverse_transform(user_pred)
+
+#         # Return predicted personality
+#         return jsonify({'username': username, 'personality': user_personality[0]})
+    
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 500
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+# app = Flask(__name__)
+
+# # Initialize NLTK components
+# nltk.download('wordnet')
+# nltk.download('stopwords')
+
 # Load model, vectorizer, and encoder
 model_path = 'linear_svc_model.pkl'
 vectorizer_path = 'tfidf_vectorizer.pkl'
@@ -44,12 +103,10 @@ def clean_user_posts(username):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return 'Welcome to Personality Prediction from Instagram Posts!'
 
-@app.route('/', methods=['POST'])
-def predict():
-    username = request.form['username']
-
+@app.route('/predict/<username>', methods=['GET'])
+def predict(username):
     if not username:
         return jsonify({'error': 'Username not provided.'}), 400
 
@@ -73,4 +130,4 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
